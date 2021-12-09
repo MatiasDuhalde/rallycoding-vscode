d70e841cc3b6c3fde498154025c65a4e087ddf01
Matias Duhalde <matias.duhalde@uc.cl>
2021-12-09 18:36:09 -0300

    :zap: Tree-shake imports

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,15 +1,15 @@
-import * as path from 'path';
+import { resolve as pathResolve } from 'path';
 import { runTests } from '@vscode/test-electron';
 
 const main = async () => {
   try {
     // The folder containing the Extension Manifest package.json
     // Passed to `--extensionDevelopmentPath`
-    const extensionDevelopmentPath = path.resolve(__dirname, '../../');
+    const extensionDevelopmentPath = pathResolve(__dirname, '../../');
 
     // The path to test runner
     // Passed to --extensionTestsPath
-    const extensionTestsPath = path.resolve(__dirname, './suite/index');
+    const extensionTestsPath = pathResolve(__dirname, './suite/index');
 
     // Download VS Code, unzip it and run the integration test
     await runTests({ extensionDevelopmentPath, extensionTestsPath });
--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -1,4 +1,4 @@
-import * as path from 'path';
+import { resolve as pathResolve } from 'path';
 import * as Mocha from 'mocha';
 import * as glob from 'glob';
 
@@ -9,7 +9,7 @@ export const run: () => Promise<void> = () => {
     color: true,
   });
 
-  const testsRoot = path.resolve(__dirname, '..');
+  const testsRoot = pathResolve(__dirname, '..');
 
   return new Promise((resolve, reject) => {
     glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
@@ -18,7 +18,7 @@ export const run: () => Promise<void> = () => {
       }
 
       // Add files to the test suite
-      files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
+      files.forEach((f) => mocha.addFile(pathResolve(testsRoot, f)));
 
       try {
         // Run the mocha test
